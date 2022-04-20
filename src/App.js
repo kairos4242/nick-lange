7ef5347e99da1a95ad98584e359a0c2bb161b097
Riley Ward <riley4242@hotmail.com>
2022-04-20 14:32:50 -0600

    Use react-responsive to render properly on small screens

--- src/App.js
+++ src/App.js
@@ -3,22 +3,35 @@ import VideoBg from "reactjs-videobg";
 import concertbwmp4 from './videos/concert_bw.mp4'
 import poster from "./images/poster.png";
 import SidebarParent from './components/SidebarParent'
+import MediaQuery from 'react-responsive'
 
 
 
 function App() {
   return (
     <div className="App">
-      <VideoBg poster={poster}>
-        <VideoBg.Source src={concertbwmp4} type="video/mp4" />
-      </VideoBg>
-      <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        <p>
-          <stylized> {/*NICK LANGE*/} </stylized>
-        </p>
-        <SidebarParent/>
-      </header>
+      <MediaQuery maxWidth={1224}>
+        <header className="App-header-mobile">
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
+          <p>
+            <stylized> {/*NICK LANGE*/} </stylized>
+          </p>
+          <SidebarParent/>
+        </header>
+      </MediaQuery>
+      <MediaQuery minWidth={1224}>
+        <VideoBg poster={poster}>
+          <VideoBg.Source src={concertbwmp4} type="video/mp4" />
+        </VideoBg>
+        <header className="App-header">
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
+          <p>
+            <stylized> {/*NICK LANGE*/} </stylized>
+          </p>
+          <SidebarParent/>
+        </header>
+      </MediaQuery>
+      
     </div>
   );
 }
