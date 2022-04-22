a37b0eddc1c4a525b06a2108dcb6f76e59341e08
atwigandberries <76791008+atwigandberries@users.noreply.github.com>
2022-04-21 20:05:31 -0600

    Fixed vid not showing on small screens
    ...../....

--- src/App.js
+++ src/App.js
@@ -6,35 +6,22 @@ import SidebarParent from './components/SidebarParent'
 import MediaQuery from 'react-responsive'
 import PayPalButton from './components/PayPalButton';
 
-
+const mobileBreak=768
 
 function App() {
   return (
     <div className="App">
-      <MediaQuery maxWidth={768}>
-        <header className="App-header-mobile">
-          {/*<img src={logo} className="App-logo" alt="logo" />*/}
-          <p>
-            <stylized> {/*NICK LANGE*/} </stylized>
-          </p>
-          <SidebarParent/>
-          <PayPalButton/>
-        </header>
-      </MediaQuery>
-      <MediaQuery minWidth={768}>
-        <VideoBg poster={poster}>
-          <VideoBg.Source src={concertbwmp4} type="video/mp4" />
-        </VideoBg>
-        <header className="App-header">
-          {/*<img src={logo} className="App-logo" alt="logo" />*/}
-          <p>
-            <stylized> {/*NICK LANGE*/} </stylized>
-          </p>
-          <SidebarParent/>
-          <PayPalButton/>
-        </header>
-      </MediaQuery>
-      
+      <VideoBg poster={poster}>
+        <VideoBg.Source src={concertbwmp4} type="video/mp4" />
+      </VideoBg>
+      <header className="App-header">
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        <p>
+          <stylized> {/*NICK LANGE*/} </stylized>
+        </p>
+        <SidebarParent/>
+        <PayPalButton/>
+      </header>
     </div>
   );
 }
